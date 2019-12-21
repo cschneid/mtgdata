2a435fe09a2d4ab68a938801d22f6f6fd7786950
Chris Schneider <chris@scoutapp.com>
2019-12-20 22:04:25 -0700

    Fix up deploy w/ real values

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "~> 3.11.2"
 
 set :application, "mtgdata"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:cschneid/mtgdata.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/mtgdata"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
