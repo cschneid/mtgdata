d9be4a6cdff83d8efd6b56cf7d072314d7e2d96d
Chris Schneider <chris@scoutapp.com>
2019-12-20 22:12:38 -0700

    exceptions

--- app/models/import/koda_buylist.rb
+++ app/models/import/koda_buylist.rb
@@ -3,19 +3,18 @@ module Import
   class KodaBuylist
     def self.import(vendor, valid_on, file)
       errors = []
-      begin
-        sets = MagicSet.cached
+      sets = MagicSet.cached
+      i = 0
 
-        i = 0
+      batch = BatchedWork.new(batch_size: 50) do |batch|
+        puts "\nRunning batch (#{i}-#{i+batch.size})"
+        Buylist.create!(batch)
+        i = i + batch.size
+      end
 
-        batch = BatchedWork.new(batch_size: 50) do |batch|
-          puts "\nRunning batch (#{i}-#{i+batch.size})"
-          Buylist.create!(batch)
-          i = i + batch.size
-        end
-
-        csv = CSV.new(File.read(file), headers: true)
-        csv.each do |row|
+      csv = CSV.new(File.read(file), headers: true)
+      csv.each do |row|
+        begin
           name = row["Card Name"].strip
           set = row["Card Set"].strip
           foil = row["NF/F"].present?
@@ -33,9 +32,9 @@ module Import
             rarity: rarity,
           }
           printing = Printing.where(printing_args).first
-          if printing.nil?
-            printing = Printing.create!(printing_args.dup)
-          end
+          # if printing.nil?
+          # printing = Printing.create!(printing_args.dup)
+          # end
           raise "Missing Printing" if printing.nil?
 
           batch.add_to_batch(
